fac50d2ee74339b19f015b99fd3353442aebf820
sxfrul <muhdsafrul@gmail.com>
2023-12-31 14:42:40 +0800

    + 16 channel support - 8 channels support

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from adafruit_servokit import ServoKit
 
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
+kit = ServoKit(channels=16)
 
 kit.servo[0].angle = 180
 kit.continuous_servo[1].throttle = 1
